4950787244d4083c53f58cf349a7d43546d39fee
AutoPR <autopr@irgolic.com>
2023-08-07 07:08:26 +0000

    Create account.py under Models folder

---
+++ Models/account.py
@@ -0,0 +1,21 @@
+class Account:
+    @staticmethod
+    def upgrade(uid: int) -> None:
+        # Implementation of the `upgrade` method
+
+    @staticmethod
+    def getTier(uid: int) -> Tier:
+        # Implementation of the `getTier` method
+```
+
+Here are the contents of the file "account.py":
+
+```
+class Account:
+    @staticmethod
+    def upgrade(uid: int) -> None:
+        pass
+
+    @staticmethod
+    def getTier(uid: int) -> Tier:
+        pass
