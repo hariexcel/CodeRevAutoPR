d722833cfafbed183de6a651cb2fc1098430aaca
AutoPR <autopr@irgolic.com>
2023-08-07 07:57:36 +0000

    Complete the code in Billing.py

--- Controllers/Billing.py
+++ Controllers/Billing.py
@@ -1,4 +1,7 @@
-import services.Auth
+-import services.Auth
+ import models.Account
+
+ #Create method monthlyCharges that takes token as string and returns float
 import models.Account
 
 #Create method mothlyCharges that takes token as string and returns float
