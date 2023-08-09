65c4c932b35287e53e819cc670e26e46629d5c9c
AutoPR <autopr@irgolic.com>
2023-08-09 10:39:01 +0000

    Implement login class and method

--- Controllers/login.py
+++ Controllers/login.py
@@ -2,10 +2,8 @@ class login:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-    
+
     def authenticate(self):
         # Add authentication logic here
-
-        def authenticate(self):
-        # Add authentication logic here
+        pass
         pass
