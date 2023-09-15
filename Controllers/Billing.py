0cd508f3d0c2792caf2f584457caca4530aac507
hariexcel <hari.excel@gmail.com>
2023-09-15 16:17:57 +0530

    Update Billing.py

--- Controllers/Billing.py
+++ Controllers/Billing.py
@@ -6,3 +6,5 @@ import models.Account
 
 # Create method monthlyCharges that takes token as string and returns float
 def monthlyCharges(token):
+
+# Create method yearlyCharges that takes toen as string and returns float
